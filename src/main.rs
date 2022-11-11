17b1433714c4cf2beddec4fe221b5d7442c4a56a
rayanpiro <rubensangria@gmail.com>
2022-11-11 13:13:38 +0100

    Refactoring and adding oneshot read mode

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use std::{collections::HashMap, fmt::Debug, fs, marker::PhantomData, time::Durat
 use tokio_cron_scheduler::{JobScheduler, Job};
 
 use std::sync::{Arc};
+use tokio::sync::Mutex;
 fn read_tags<T, C, S>(path: &str) -> Vec<Arc<dyn TTag>>
 where
     T: THardDevice<C, S> + Clone + Send + Sync + 'static,
@@ -29,13 +30,14 @@ where
         .into_iter()
         .nth(0)
         .unwrap();
+    
+    let mutex_device = Arc::new(Mutex::new(T::new(connection)));
 
     ini_parser::read_file::<S>(&(format!("{}/publishers.ini", &path)))
         .into_iter()
         .map(|t| {
-            let device = T::new(connection.clone());
             let tag: Arc<dyn TTag> = Arc::new(TagId {
-                handler: Arc::new(device),
+                handler: Arc::clone(&mutex_device),
                 tag: Arc::new(t),
                 _phantom: PhantomData,
             });
@@ -44,10 +46,7 @@ where
         .collect()
 }
 
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let sched = JobScheduler::new().await.unwrap();
+fn from_ini() -> Vec<Arc<dyn  TTag>> {
     let mut tags: Vec<Arc<dyn TTag>> = Vec::new();
 
     for folder in INI_PROTOCOL_FOLDERS {
@@ -72,30 +71,56 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    use futures::FutureExt;
+    tags
+}
+
+async fn daemon_mode(tags: Vec<Arc<dyn TTag>>) {
+    let sched = JobScheduler::new().await.unwrap();
+
     for t in tags.iter() {
+        let seconds = t.get_tag().get_freq().to_seconds();
         let t = t.clone();
-        let job = Job::new_repeated_async(Duration::from_secs(t.get_tag().get_freq().to_seconds()), move |_uuid, _l| Box::pin({
+        let job = Job::new_repeated_async(Duration::from_secs(seconds), move |_uuid, _l| Box::pin({
             let t = t.clone();
             async move {
                 dbg!(t.get_tag().get_name());
-                t.read().then(|f| async { dbg!(f) }).await;
+                while let Err(_) = dbg!(t.read().await) {
+                    println!("Trying to read again tag {}!", t.get_tag().get_name());
+                }
             }
         })).unwrap();
         sched.add(job).await.unwrap();
     };
 
-    let _ = sched.start().await.unwrap().await;
+    dbg!(sched.start().await.unwrap().await);
+}
 
-    // loop {
-    //     use futures::FutureExt;
-    //     let h1 = tags
-    //         .iter()
-    //         .map(|t| t.read().then(|f| async { dbg!(f.unwrap()) }));
+async fn one_shot_read(tags: Vec<Arc<dyn TTag>>, tag_to_read: &str) {
+    let filtered_tags = tags.iter()
+        .filter(|t| t.get_tag().get_name() == tag_to_read);
+        
+    for t in filtered_tags {
+        let t = t.clone();
+        let res = t.read().await;
+        match res {
+            Ok(x) => print!("{}", x.value.to_string()),
+            Err(_) => print!("Error"),
+        };
+    }
+}
 
-    //     use futures::future::join_all;
-    //     let _ = dbg!(join_all(h1).await);
-    //     let _ = tokio::time::sleep(tokio::time::Duration::new(2, 0)).await;
-    // }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let tags: Vec<Arc<dyn TTag>> = from_ini();
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 2 {
+        one_shot_read(tags, args.get(1).unwrap()).await;
+    }
+    else {
+        daemon_mode(tags).await;
+    }
+    
     Ok(())
 }
